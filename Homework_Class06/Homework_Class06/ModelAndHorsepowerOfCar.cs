3b14fae46576440940967e1e268770fd1ae165f2
PetarDimitrov94 <petar.dimitrov@hotmail.com>
2025-05-20 22:46:15 +0200

    Homework_Class06

---
+++ Homework_Class06/Homework_Class06/ModelAndHorsepowerOfCar.cs
@@ -0,0 +1,16 @@
+﻿namespace Homework_Class06
+{
+    public class ModelAndHorsepowerOfCar
+    {
+        public string Model { get; set; }
+        public double HorsePower { get; set; }
+
+        public void PrintInfo (List<ModelAndHorsepowerOfCar> list)
+        {
+            foreach(ModelAndHorsepowerOfCar item in  list)
+            {
+                Console.WriteLine ($"{item.Model}, {item.HorsePower}");
+            }
+        }
+    }
+}
